9e378674d57a857d0ce55a051a04ad743b6ab6a8
chensi <Kelsey_Jane@163.com>
2016-10-24 21:44:28 +0800

    thirdCommit

---
+++ app/src/main/java/com/example/think/pekingweather/SelectCity.java
@@ -0,0 +1,42 @@
+package com.example.think.pekingweather;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+
+/**
+ * Created by think on 2016/10/18.
+ */
+public class SelectCity extends Activity implements View.OnClickListener{
+    private ImageView mBackBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.select_city);
+
+        mBackBtn = (ImageView) findViewById(R.id.title_back);
+        mBackBtn.setOnClickListener(this);
+
+        //setContentView(R.layout.select_city);
+
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.title_back:
+                Intent i = new Intent();
+                i.putExtra("cityCode","101160101");
+                setResult(RESULT_OK,i);
+                finish();
+                break;
+            default:
+                break;
+        }
+    }
+}
